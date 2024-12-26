4319918c9e0eec18b3b515a6e24627b439cb09ff
Maykell Sánchez Romero <kellsaro@gmail.com>
2024-12-26 17:14:46 -0500

    Add remaining CRUD methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,44 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
+
   def index
     @products = Product.all
   end
+
+  def show; end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    return redirect_to(@product) if @product.save
+
+    render :new, status: :unprocessable_entity
+  end
+
+  def edit; end
+
+  def update
+    return redirect_to(@product) if @product.update(product_params)
+
+    render :edit, status: :unprocessable_entity
+  end
+
+  def destroy
+    @product.destroy
+
+    redirect_to products_path
+  end
+
+  private
+    def product_params
+      params.expect(product: [:name])
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
 end
