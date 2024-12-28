5c1cb4e15d86c778dc31942aa7c142011b6c8238
Maykell Sánchez Romero <kellsaro@gmail.com>
2024-12-27 19:41:33 -0500

    Add Rich Text field with Action Text

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -37,7 +37,7 @@ class ProductsController < ApplicationController
 
   private
     def product_params
-      params.expect(product: [:name])
+      params.expect(product: [:name, :description])
     end
 
     def set_product
