342405d342ad12ffa734f5ce0220110c54cdbb13
Maykell Sánchez Romero <kellsaro@gmail.com>
2025-01-08 19:39:00 -0500

    Notify subscribers after commit if back in stock

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,17 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 
+  after_update_commit :notify_subscribers, if: :back_in_stock?
+
+  private
+
+    def back_in_stock?
+      inventory_count_previously_was.zero? && inventory_count > 0
+    end
+
+    def notify_subscribers
+      subscribers.each do |subscriber|
+        ProductMailer.with(product: self, subscriber: subscriber).in_stock.deliver_later
+      end
+    end
 end
